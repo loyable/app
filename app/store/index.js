4aa918ce5f6e0bdfb91eba5ab1efdc5734840ebf
Riccardo Sacco <rickybag99@gmail.com>
2019-03-13 01:10:47 +0100

    initial

--- app/store/index.js
+++ app/store/index.js
@@ -3,12 +3,6 @@ import thunk from "redux-thunk";
 
 import RootReducer from "./reducers/RootReducer";
 
-import { reduxFirestore, getFirestore } from "redux-firestore";
-
-import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-
-import firebase from "../config/firebase";
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
