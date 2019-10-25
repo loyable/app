cab762d1483634fa240513a509f40e5e6f63888b
rickybag <rickybag99@gmail.com>
2019-10-25 16:04:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import { AppRegistry } from "react-native";
 
 import App from "./App";
 
+//Import app name
 import { name } from "./app.json";
 
+//Import Redux store
 import { store } from "./app/store";
+
+//Import Redux Provider component
 import { Provider } from "react-redux";
 
+//Import Utils component
 import Utils from "./app/config/utils";
+
 class Root extends Component {
   componentDidMount() {
     //Check for updates
