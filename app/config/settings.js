6a9c7da8b73ece36326c64a6e3499957311a84fc
Riccardo Sacco <rickybag99@gmail.com>
2022-07-12 17:49:09 +0200

    updated

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,12 +1,12 @@
 export default {
   version: "1.0.0",
   url: {
-    api: "https://api.loyable.com/api/v1",
-    watch: "https://api.loyable.com:3500"
+    api: "http://localhost:4500/api/v1",
+    watch: "http://localhost:3500",
   },
   popupUpdate: {
     title: "Aggiornamento disponibile!",
     subtitle: "Dopo l'aggiornamento potrai continuare ad utilizzare l'app",
-    button: "Aggiorna"
-  }
+    button: "Aggiorna",
+  },
 };
